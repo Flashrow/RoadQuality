b54d5ed909ad08c899595a6cee1d5c13f098d36c
Kamil <kamilpier08@gmail.com>
2021-04-30 12:57:18 +0200

    spring init

---
+++ RoadQualityBackend/src/main/java/com/roadquality/demo/RoadQualityApplication.java
@@ -0,0 +1,13 @@
+package com.roadquality.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadQualityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoadQualityApplication.class, args);
+    }
+
+}
---
+++ RoadQualityBackend/src/test/java/com/roadquality/demo/RoadQualityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roadquality.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadQualityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
